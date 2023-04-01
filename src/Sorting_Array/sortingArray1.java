d3ab9a85d354f7ebdddcb76d7a5955ad828b288d
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2023-04-01 15:09:35 +0900

    Selection sorting

--- src/Sorting_Array/sortingArray1.java
+++ src/Sorting_Array/sortingArray1.java
@@ -27,6 +27,10 @@ public class sortingArray1 {
         System.out.println(Arrays.toString(input));
     }
     private static void selectionSorting(int[] array){
+        for (int i=0;i<array.length;i++){
+
+            for(int j=i+1;j<array.length;j++)
+        }
 
         System.out.println(Arrays.toString(array));
 
