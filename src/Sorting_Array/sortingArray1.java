22414a29670dc7432b60dc1d9ad1ab68aadd5601
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2023-04-01 15:00:01 +0900

    Bubble sorting

---
+++ src/Sorting_Array/sortingArray1.java
@@ -0,0 +1,28 @@
+package Sorting_Array;
+
+import java.util.Arrays;
+
+public class sortingArray1 {
+   static int[] input={5,2,3,1};
+//    Input: nums = [5,2,3,1]
+//    Output: [1,2,3,5]
+
+    public static void main(String[] args) {
+        sortarray(input);
+    }
+
+    private static void sortarray(int[] input) {
+        int arrayLength= input.length;
+        for(int i=0;i<arrayLength;i++){
+            for(int j=i+1;j<arrayLength;j++){
+                if(input[i]>input[j]) {
+                    int temp=input[i];
+                    input[i] = input[j];
+                    input[j]=temp;
+                }
+            }
+
+        }
+        System.out.println(Arrays.toString(input));
+    }
+}
